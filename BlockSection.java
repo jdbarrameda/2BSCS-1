0df229ad86c01a53e8a476f3e98fb424bd4fbfcf
jdbarrameda <133651151+jdbarrameda@users.noreply.github.com>
2024-02-05 04:36:38 +0000

    xxx

---
+++ BlockSection.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class BlockSection{
+	private String blockCode;
+	private String adviser;
+	private List<Student> studentList;
+	
+	//setter methods
+	public void setBlockCode(String blockCode){
+		this.blockCode = blockCode;
+	}
+	
+	public void setAdviser(String adviser){
+		this.adviser = adviser;
+	}
+	
+	public void setStudentList(List<Student> studentList){
+		this.studentList = studentList;
+	}
+
+	//getter methods
+	
+	public String getBlockCode(){
+		return blockCode;
+	}
+
+	public String getAdviser(){
+		return adviser;
+	}
+
+ 	public List<Student> getStudentList(){
+		return studentList;
+	}
+}
